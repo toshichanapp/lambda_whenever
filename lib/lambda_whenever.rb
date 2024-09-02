83c30b153204b6fcd8d95075e299ea7ff02c59cf
toshichanapp <toshichanapp@gmail.com>
2024-09-02 14:14:23 +0900

    bundle gem lambda_whenever --no-coc --no-mit --no-git

---
+++ lib/lambda_whenever.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lambda_whenever/version"
+
+module LambdaWhenever
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lambda_whenever_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LambdaWhenever do
+  it "has a version number" do
+    expect(LambdaWhenever::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
